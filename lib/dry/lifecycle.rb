ea3d2cf5eb5bc9e9e24ce9d62c67348c3eff1d6b
Brian V. Hughes <brianvh89@gmail.com>
2019-10-28 15:11:06 -0700

    Add main Lifecycle class
    This acts as the main Superclass that is used to define and operate a
    Lifecycle state machine. Wraps access to the underlying Definition
    class and its transition mechanics, via the #call method.
    This method exposes a Dry::Monads::Request matcher, similar to a
    Dry::Transaction, making success vs. failure branching easier.
    Includes a set of integration Specs to verify construction and
    transition behavior.

--- lib/dry/lifecycle.rb
+++ lib/dry/lifecycle.rb
@@ -1,10 +1,48 @@
 # frozen_string_literal: true
 
+require 'dry/configurable'
+require 'dry/container'
+require 'dry/matcher'
+require 'dry/matcher/result_matcher'
+
 require 'dry/lifecycle/version'
+require 'dry/lifecycle/errors'
+require 'dry/lifecycle/definition'
 
 module Dry
-  module Lifecycle
-    class Error < StandardError; end
-    # Your code goes here...
+  def self.Lifecycle(container:)
+    Class.new(Dry::Lifecycle) do
+      config.container = container
+    end
+  end
+
+  class Lifecycle
+    extend Dry::Configurable
+
+    setting :container, {}
+
+    class << self
+      attr_reader :definition
+
+      def define(&block)
+        @definition ||= Definition.new(container: config.container, &block)
+      end
+    end
+
+    include Dry::Matcher.for(:call, with: Dry::Matcher::ResultMatcher)
+
+    def call(object, new_state)
+      _definition.enter(object, new_state)
+    end
+
+    def states
+      _definition.states
+    end
+
+    private
+
+    def _definition
+      self.class.definition
+    end
   end
 end
--- spec/dry/lifecycle_spec.rb
+++ spec/dry/lifecycle_spec.rb
@@ -1,9 +1,44 @@
+require 'spec_helper'
+
 RSpec.describe Dry::Lifecycle do
-  it "has a version number" do
-    expect(Dry::Lifecycle::VERSION).not_to be nil
+  let(:duplicate_state) { Dry::Lifecycle::DuplicateState }
+  let(:invalid_exit) { Dry::Lifecycle::InvalidExit }
+  let(:invalid_state) { Dry::Lifecycle::InvalidState }
+
+  context 'when defined with no states' do
+    let(:lifecycle) {
+      Class.new(Dry::Lifecycle) do
+        define {}
+      end.new
+    }
+
+    it 'contains no states' do
+      expect(lifecycle.states).to eq({})
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context 'when entering a new state, from an initial state' do
+    let(:lifecycle) do
+      Class.new(Dry::Lifecycle) do
+        define do
+          state(:one) { exit(:two) }
+          state(:two)
+        end
+      end.new
+    end
+    let(:object) { Struct.new(:state).new('one') }
+    let(:result) { lifecycle.call(object, :two) }
+
+    it 'returns Success, wrapping the object in the new state' do
+      expect(result.value!.state).to eq('two')
+    end
+
+    it 'yields the value to #success, when called with a block' do
+      result = lifecycle.call(object, :two) do |r|
+        r.success { |v| v }
+        r.failure {}
+      end
+      expect(result.state).to eq('two')
+    end
   end
 end
